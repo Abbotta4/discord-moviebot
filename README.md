edda7df72e8f8ddf89709784e74322db0fc3d408
Drew Abbott <abbotta4@gmail.com>
2022-07-31 19:40:47 +0200

    Add boilerplate and README
    Add boilerplate discord.py code
    Add default .gitignore
    Add basic README

---
+++ README.md
@@ -0,0 +1,11 @@
+# discord-moviebot.py
+
+A Discord bot written in Python 3 using discord.py for automating some tasks in the movie club discord
+
+#### Setup
+```
+$ python -m venv .
+$ . bin/activate
+$ pip install discord.py
+$ python discord-movieboy.py
+```
---
+++ discordpy-moviebot.py
@@ -0,0 +1,21 @@
+import discord, logging
+
+def setupLogging():
+    #logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+if __name__ == '__main__':
+    setupLogging()
+    client = MyClient()
+    client.run('redacted')
